2ee2c2833cb4366747a519e4ee2eb7d37c96e4a3
unknown <shmebulok1818@gmail,com>
2023-01-24 07:41:56 -0600

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = null "Hello Word"; 
 
 // Crea una variable booleana:
 const nuevoBool = null;
